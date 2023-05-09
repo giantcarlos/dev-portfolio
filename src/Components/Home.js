57cba4e03ebc8d4e201de73ccb25dd31f3075c87
Gian Taino Carlos <giantainocarlos@gmail.com>
2023-05-09 15:27:23 -0500

    add nav css

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+        <h1>Gian T. Carlos</h1>
+        <h3>Software Engineer / Front-End Web Developer</h3>
+    </div>
   )
 }
 
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom';
 
 function NavBar() {
   return (
-    <div>NavBar</div>
+    <nav>
+      <div className="site-title">GianTCarlos.</div>
+      <NavLink to="/">// home   </NavLink>
+      <NavLink to="/about">// about   </NavLink>
+      <NavLink to="/projects">// projects   </NavLink>
+      <NavLink to="/cv">// cv   </NavLink>
+      <NavLink to="/contact">// contact   </NavLink>
+    </nav>
   )
 }
 
