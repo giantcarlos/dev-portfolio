02f932763c4ac271169580742d9d8f519823df6e
Gian Taino Carlos <giantainocarlos@gmail.com>
2023-05-10 15:31:44 -0500

    fix resume

--- src/Components/CV.js
+++ src/Components/CV.js
@@ -1,15 +1,9 @@
 import React from 'react'
-import { Document, Page, pdfjs } from 'react-pdf';
 
 function CV() {
-    const resumeUrl = '/gianresume.pdf';
-    pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
-
   return (
     <div className='cv'>
-      <Document file={resumeUrl}>
-        <Page pageNumber={1} />
-      </Document>
+      <img src={'/resumeimage.png'} className='cv-image'/>
     </div>
   )
 }
