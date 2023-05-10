8f9eff9117221fbe711dac60e55940737bd93f07
Gian Taino Carlos <giantainocarlos@gmail.com>
2023-05-10 13:24:07 -0500

    add project cards

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -2,7 +2,35 @@ import React from 'react'
 
 function Projects() {
   return (
-    <div>Projects</div>
+    <div className='container'>
+        <div className="card">
+            <div className="card-image">
+                {/* <img src={} alt="cover"/> */}
+            </div>
+            <div className="card-title">Miniature Tracker</div>
+            <div className="card-details"></div>
+            <div className="card-details"></div>
+            <div className="card-details"></div>
+        </div>
+        <div className="card">
+            <div className="card-image">
+                {/* <img src={} alt="cover"/> */}
+            </div>
+            <div className="card-title">Scotcher</div>
+            <div className="card-details"></div>
+            <div className="card-details"></div>
+            <div className="card-details"></div>
+        </div>
+        <div className="card">
+            <div className="card-image">
+                {/* <img src={} alt="cover"/> */}
+            </div>
+            <div className="card-title">Post-Records</div>
+            <div className="card-details"></div>
+            <div className="card-details"></div>
+            <div className="card-details"></div>
+        </div>
+    </div>
   )
 }
 
