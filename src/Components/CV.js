daca213c89416584be251f8702d1b5f50be8eab1
Gian Taino Carlos <giantainocarlos@gmail.com>
2023-05-10 04:25:32 -0500

    import resume pdf

--- src/Components/CV.js
+++ src/Components/CV.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Document, Page, pdfjs } from 'react-pdf';
 
 function CV() {
+    const resumeUrl = '/gianresume.pdf';
+    pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+
   return (
-    <div>CV</div>
+    <div className='cv'>
+      <Document file={resumeUrl}>
+        <Page pageNumber={1} />
+      </Document>
+    </div>
   )
 }
 
