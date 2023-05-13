2f978457267c70b5afce9fed4ff1aa9b70f666f2
Gian Taino Carlos <giantainocarlos@gmail.com>
2023-05-13 14:27:49 -0500

    change nav logo

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,13 +1,17 @@
 import Hamburger from 'hamburger-react';
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom';
 
 function NavBar() {
-    
+    const [active, setActive] = useState('nav-links')
+    const navToggle = () => {
+        active === 'nav-links' ? setActive('nav-links-active') : setActive('nav-links');
+      }
+
   return (
     <nav>
-      <div className="site-title">GianTCarlos.<span className="highlight">_</span></div>
-      <div className="nav-links">
+      <div className="site-title">gtc.<span className="highlight">_</span></div>
+      <div className={active}>
         <NavLink to="/">// home   </NavLink>
         <NavLink to="/bio">// bio   </NavLink>
         <NavLink to="/projects">// projects   </NavLink>
@@ -15,7 +19,7 @@ function NavBar() {
         <NavLink to="/contact">// contact   </NavLink>
       </div>
       <div className="hamburger" >
-        <Hamburger/>
+        <Hamburger onClick={navToggle}/>
       </div>
     </nav>
   )
