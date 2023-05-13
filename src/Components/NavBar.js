64496cbd43cadf548458f2d140bbd2d4e4ced4ce
Gian Taino Carlos <giantainocarlos@gmail.com>
2023-05-13 16:11:08 -0500

    add responsive bar ease transition

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -3,23 +3,29 @@ import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom';
 
 function NavBar() {
+    const [isOpen, setOpen] = useState(false)
     const [active, setActive] = useState('nav-links')
     const navToggle = () => {
         active === 'nav-links' ? setActive('nav-links-active') : setActive('nav-links');
+        console.log(active);
       }
+    const navOff = () => {
+        setActive('nav-links')
+        setOpen(false)
+    }
 
   return (
     <nav>
       <div className="site-title">gtc.<span className="highlight">_</span></div>
-      <div className={active}>
+      <div className={active} onClick={navOff}>
         <NavLink to="/">// home   </NavLink>
         <NavLink to="/bio">// bio   </NavLink>
         <NavLink to="/projects">// projects   </NavLink>
         <NavLink to="/cv">// cv   </NavLink>
         <NavLink to="/contact">// contact   </NavLink>
       </div>
-      <div className="hamburger" >
-        <Hamburger onClick={navToggle}/>
+      <div className="hamburger" onClick={navToggle}>
+        <Hamburger toggled={isOpen} toggle={setOpen}/>
       </div>
     </nav>
   )
