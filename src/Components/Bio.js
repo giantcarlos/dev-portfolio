df90ba58b57802bf993aa6e7aef1c64bb6b7dad1
Gian Taino Carlos <giantainocarlos@gmail.com>
2023-05-14 03:37:17 -0500

    edit bio

--- src/Components/Bio.js
+++ src/Components/Bio.js
@@ -6,7 +6,7 @@ function Bio() {
       <img className="me" src="/bolo.jpg" alt="" />
       <p>Manila-born, Texas-based software engineer with a background in Fashion Merchandising. I bring a technical mindset and a keen eye for aesthetics and UI/UX design to my work. While I'm trained as a full-stack developer, my true passion lies in front-end development, where I can combine my love for coding and design.</p>
       <p>Beyond the world of code, I find solace in music. Having toured as a musician in my youth, I now DJ in the local Dallas area, allowing me to stay connected to my musical roots. On top of that, I'm an avid long-distance runner, having completed the Dallas Marathon four times and currently training for my fifth.</p>
-      <p>In my downtime, you'll often find me engaging in analog/paper/tabletop gaming. Dungeons & Dragons and Magic: The Gathering are my preferred choices, as I appreciate the immersive and interactive experiences they offer. I value the creativity and social connections these games foster.</p>
+      <p>In my downtime, you'll often find me engaging in paper/tabletop gaming. Dungeons & Dragons and Magic: The Gathering are my preferred choices, as I value the creativity and social connections these games foster.</p>
     </div>
   )
 }
