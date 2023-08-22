23c21fa669dec95c9d515c058380d23447975f72
Gian Taino Carlos <giantainocarlos@gmail.com>
2023-08-22 13:28:01 -0500

    add Dentonpalooza to projects

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -6,23 +6,20 @@ function Projects() {
     <div className='container'>
         <div className="card">
             <div className="card-image">
-                <img src={'https://miro.medium.com/v2/resize:fit:1400/format:webp/1*lJwrn2GVmSOiMSdHgzoc3A.png'} alt="cover"/>
+                <img src={'https://i.imgur.com/a7dypvu.png'} alt="cover"/>
             </div>
             <div className='text-container'>
-              <div className="card-title">Post-Records</div>
-              <div className="card-details">Online retail storefront for a specialty record store dealing with rare and out of print post-punk records.</div>
-              <div className="card-details">● Developed a JavaScript/React front-end application with a mock JSON API back-end.</div>
-              <div className="card-details">● Designed visually appealing layouts, cards, and buttons with custom CSS styling.</div>
-              <div className="card-details">● Implemented UI/UX design principles for a seamless customer online retail experience.</div>
+              <div className="card-title">Dentonpalooza</div>
+              <div className="card-details">Freelance web development project for the Dentonpalooza festival in Denton, TX.</div>
+              <div className="card-details">● Leveraged React and CSS to construct an interactive, dynamic website, enhancing user experience.</div>
+              <div className="card-details">● Translated the client's artistic direction into a captivating UI/UX, while seamlessly incorporating sponsor identities.</div>
+              <div className="card-details">● Acquired firsthand exposure to professional and non-profit development dynamics, broadening my skill set.</div>
             </div>
-            <Link to={'https://post-records-client.onrender.com/'} target="_blank">
+            <Link to={'https://giantcarlos.github.io/dentonpalooza23/'} target="_blank">
               <button className="button">Live Link</button>
             </Link>
-            <Link to={'https://github.com/giantcarlos/post-records-storefront'} target="_blank">
+            <Link to={'https://github.com/giantcarlos/dentonpalooza23'} target="_blank">
               <button className="button">Github</button>
-            </Link>
-            <Link to={'https://www.youtube.com/watch?v=D04XhApmUg8'} target="_blank">
-              <button className="button">Demo Video</button>
             </Link>
         </div>
         <div className="card">
@@ -63,6 +60,27 @@ function Projects() {
               <button className="button">Github</button>
             </Link>
             <Link to={'https://www.youtube.com/watch?v=uLI89WSmleM'} target="_blank">
+              <button className="button">Demo Video</button>
+            </Link>
+        </div>
+        <div className="card">
+            <div className="card-image">
+                <img src={'https://miro.medium.com/v2/resize:fit:1400/format:webp/1*lJwrn2GVmSOiMSdHgzoc3A.png'} alt="cover"/>
+            </div>
+            <div className='text-container'>
+              <div className="card-title">Post-Records</div>
+              <div className="card-details">Online retail storefront for a specialty record store dealing with rare and out of print post-punk records.</div>
+              <div className="card-details">● Developed a JavaScript/React front-end application with a mock JSON API back-end.</div>
+              <div className="card-details">● Designed visually appealing layouts, cards, and buttons with custom CSS styling.</div>
+              <div className="card-details">● Implemented UI/UX design principles for a seamless customer online retail experience.</div>
+            </div>
+            <Link to={'https://post-records-client.onrender.com/'} target="_blank">
+              <button className="button">Live Link</button>
+            </Link>
+            <Link to={'https://github.com/giantcarlos/post-records-storefront'} target="_blank">
+              <button className="button">Github</button>
+            </Link>
+            <Link to={'https://www.youtube.com/watch?v=D04XhApmUg8'} target="_blank">
               <button className="button">Demo Video</button>
             </Link>
         </div>
