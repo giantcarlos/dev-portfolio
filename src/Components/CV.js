aedd5bf08eb7d4d491eeb732e4f325c7bd302cb4
Gian Taino Carlos <giantainocarlos@gmail.com>
2023-08-24 23:08:14 -0500

    reduce cv image file size

--- src/Components/CV.js
+++ src/Components/CV.js
@@ -3,7 +3,7 @@ import React from 'react'
 function CV() {
   return (
     <div className='cv'>
-      <img src={'https://i.imgur.com/DebZLBt.png'} className='cv-image'/>
+      <img src={'https://i.imgur.com/7d2XARJ.png'} className='cv-image'/>
     </div>
   )
 }
