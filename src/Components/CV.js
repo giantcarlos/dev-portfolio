c227fa78371678653b84836ac2d67148b14da121
Gian Taino Carlos <giantainocarlos@gmail.com>
2023-09-08 13:40:38 -0500

    resume edit

--- src/Components/CV.js
+++ src/Components/CV.js
@@ -3,7 +3,7 @@ import React from 'react'
 function CV() {
   return (
     <div className='cv'>
-      <img src={'https://i.imgur.com/7d2XARJ.png'} className='cv-image'/>
+      <img src={'https://i.imgur.com/OhX1bhG.png'} className='cv-image'/>
     </div>
   )
 }
