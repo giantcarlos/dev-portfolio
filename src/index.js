92751b430c61e8fe98b1bcf4be9374560de14135
Gian Taino Carlos <giantainocarlos@gmail.com>
2023-09-19 15:53:10 -0500

    change BrowserRouter to HashRouter

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <HashRouter basename="/">
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
 
 reportWebVitals();
