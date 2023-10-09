035beab4ae707c898e8e1c736cd4e342d1d484bb
Gian Taino Carlos <giantainocarlos@gmail.com>
2023-10-09 16:38:21 -0500

    add resume pdf download

--- src/Components/CV.js
+++ src/Components/CV.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import React from 'react';
+import ResumePdf from '../gianresume.pdf';
 
 function CV() {
   return (
-    <div className='cv'>
-      <img src={'/gianresume.png'} className='cv-image'/>
-    </div>
+    <>
+      <a
+        className="pdf-button"
+        href={ResumePdf}
+        download="gianresume.pdf"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <button className="button2">Download PDF</button>  
+      </a>
+      <div className='cv'>
+        <img src={'/gianresume.png'} className='cv-image'/>
+      </div>
+    </>
   )
 }
 
