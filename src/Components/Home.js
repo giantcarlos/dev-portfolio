b8209e0ea928976eec3c5c875c78747c9a8f2718
Gian Taino Carlos <giantainocarlos@gmail.com>
2023-11-20 22:04:47 -0600

    edit sub header to designer

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,7 +4,7 @@ function Home() {
   return (
     <div className="home">
         <h1>GIAN T. CARLOS</h1>
-        <h3>SOFTWARE ENGINEER / FRONT-END WEB DEVELOPER</h3>
+        <h3>FRONT-END WEB DEVELOPER / DESIGNER</h3>
     </div>
   )
 }
