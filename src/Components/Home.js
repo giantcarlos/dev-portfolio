79a2f94f96f7cd162ad386446b784a971a794b74
Gian Taino Carlos <giantainocarlos@gmail.com>
2024-09-04 12:53:55 -0500

    edit home sub-title

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,7 +4,7 @@ function Home() {
   return (
     <div className="home">
         <h1>GIAN T. CARLOS</h1>
-        <h2>FRONT-END WEB DEVELOPER / DESIGNER</h2>
+        <h2>WEB DEVELOPER / UI DESIGNER</h2>
     </div>
   )
 }
