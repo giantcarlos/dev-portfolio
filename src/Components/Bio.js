b03274376d8b5d844eac81a36317e0b1b76f7cf2
Gian Taino Carlos <giantainocarlos@gmail.com>
2025-01-13 14:18:39 -0600

    update bio text to add WCAG 2.2

--- src/Components/Bio.js
+++ src/Components/Bio.js
@@ -4,7 +4,7 @@ function Bio() {
   return (
     <div className="bio">
       <img className="me" src="bolo.jpg" alt="headshot photo of Gian Carlos" />
-      <p>Manila-born, Texas-based software engineer specializing in front-end web development, UI design, and web accessibility. Experienced in JavaScript, React, Redux, Ruby, Rails, HTML, CSS, and Figma, with a strong commitment to crafting intuitive and inclusive user experiences. Up to date with current WCAG standards, ensuring accessibility in every project.</p>
+      <p>Manila-born, Texas-based software engineer specializing in front-end web development, UI design, and web accessibility. Experienced in JavaScript, React, Redux, Ruby, Rails, HTML, CSS, and Figma, with a strong commitment to crafting intuitive and inclusive user experiences. Up to date with current WCAG 2.2 standards, ensuring accessibility in every project.</p>
       <p>Beyond the world of code, I am a former touring musician turned local Dallas DJ. A dedicated long-distance runner with four Dallas Marathon finishes, currently gearing up for the fifth. During downtime, I indulge in Dungeons & Dragons and Magic: The Gathering, as I value the creativity and social connections these games foster.</p>
     </div>
   )
