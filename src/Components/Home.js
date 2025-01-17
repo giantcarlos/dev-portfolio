207a3b7347fb359f3cff6524b80ebd2cec5f68d4
Gian Taino Carlos <giantainocarlos@gmail.com>
2025-01-17 15:16:57 -0600

    Add accessibility to home page subtitle.

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,7 +4,7 @@ function Home() {
   return (
     <div className="home">
         <h1>GIAN T. CARLOS</h1>
-        <h2>FRONT-END DEVELOPER / ACCESSIBILITY SPECIALIST</h2>
+        <h2>FRONT-END DEVELOPER / WEB ACCESSIBILITY SPECIALIST</h2>
     </div>
   )
 }
