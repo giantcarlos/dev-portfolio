d14f0a31ac2ea8220dd7f3eff2fe79fd235dad1a
Gian Taino Carlos <giantainocarlos@gmail.com>
2026-04-21 21:25:53 -0500

    Add animated circle to home

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,7 +4,8 @@ function Home() {
   return (
     <div className="home">
         <div className="name">
-          <h1>GIAN T. CARLOS</h1>
+          <span className="home-circle" aria-hidden="true"></span>
+          <h1><span className="home-title-text">GIAN T. CARLOS</span></h1>
         </div>
         <h2>WEB DEVELOPER / UI DESIGNER / ACCESSIBILITY SPECIALIST</h2>
     </div>
