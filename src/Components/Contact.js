9876dd7280818cf754160004a015801c5496fedb
Gian Taino Carlos <giantainocarlos@gmail.com>
2026-05-08 23:07:07 -0500

    Bold contact section text

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -8,24 +8,24 @@ function Contact() {
     <>
       <Divider />
       <Fade>
-      <div>
+        <div>
           <div className='contact-text'>
-              <h3>Available for web development and accessibility audit work.</h3>
-              <h3>Shoot me an email with your project idea!</h3><br />
-              <h3>email: <a href="mailto:giantainocarlos@gmail.com" className="email-link">giantainocarlos@gmail.com</a></h3>
+            <h3>Available for web development and accessibility audit work.</h3>
+            <h3>Shoot me an email with your project idea!</h3>
+            <h3>email: <a href="mailto:giantainocarlos@gmail.com" className="email-link">giantainocarlos@gmail.com</a></h3>
           </div>
           <div className='icons'>
-              <Link to={'https://www.linkedin.com/in/gian-taino-carlos/'} target="_blank">
-                  <img className="logo" src={'/linkedinicon.png'} alt="LinkedIn icon."/>
-              </Link>
-              <Link to={'https://github.com/giantcarlos'} target="_blank">
-                  <img className="logo2" src={'githubicon.png'} alt="Github icon."/>  
-              </Link>
-              <Link to={'https://www.instagram.com/vestments/'} target="_blank">
-                  <img className="logo" src={'instagramicon.png'} alt="Instagram icon."/>
-              </Link>
+            <Link to={'https://www.linkedin.com/in/gian-taino-carlos/'} target="_blank">
+              <img className="logo" src={'/linkedinicon.png'} alt="LinkedIn icon."/>
+            </Link>
+            <Link to={'https://github.com/giantcarlos'} target="_blank">
+              <img className="logo2" src={'githubicon.png'} alt="Github icon."/>  
+            </Link>
+            <Link to={'https://www.instagram.com/vestments/'} target="_blank">
+              <img className="logo" src={'/instagramicon.png'} alt="Instagram icon."/>
+            </Link>
           </div>
-      </div>
+        </div>
       </Fade>
     </>
   )
